fe21dcf0a9480018307ee7ff2ff85119bc7fe004
g.skrylnikov <grisha2182@gmail.com>
2023-12-25 02:43:45 +0300

    test3

--- client.py
+++ client.py
@@ -1,26 +1,22 @@
 import socket
 
-def echo_client(message):
-    # Создаем сокет
+def send_data(message):
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    
-    # Получаем имя хоста
-    host = socket.gethostname()
+    host = "192.168.0.143"  # Замените на IP-адрес вашего сервера
     port = 12345
-    
-    # Подключаемся к серверу
-    client_socket.connect((host, port))
-    
-    # Отправляем сообщение серверу
-    client_socket.send(message.encode())
-    
-    # Получаем и выводим ответ от сервера
-    data = client_socket.recv(1024)
-    print(f"Получено от сервера: {data.decode()}")
-    
-    # Закрываем соединение
-    client_socket.close()
+
+    try:
+        client_socket.connect((host, port))
+        client_socket.send(message.encode())
+
+        data = client_socket.recv(1024)
+        print(f"Получено от сервера: {data.decode()}")
+
+    except Exception as e:
+        print(f"Произошла ошибка: {e}")
+    finally:
+        client_socket.close()
 
 if __name__ == "__main__":
     message = "Привет, сервер! Это сообщение от клиента."
-    echo_client(message)
+    send_data(message)
--- server.py
+++ server.py
@@ -1,35 +1,36 @@
 import socket
 
 def echo_server():
-    # Создаем сокет
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    
-    # Получаем имя хоста
-    host = socket.gethostname()
+    host = "192.168.0.143"
     port = 12345
-    
-    # Привязываем сокет к заданному адресу и порту
-    server_socket.bind((host, port))
-    
-    # Слушаем до 5 запросов
-    server_socket.listen(5)
-    
-    print(f"Эхо-сервер запущен на {host}:{port}...")
-    
-    while True:
-        # Принимаем подключение
-        client_socket, addr = server_socket.accept()
-        print(f"Подключено от {addr}")
-        
-        # Получаем данные от клиента и отправляем обратно
-        data = client_socket.recv(1024)
-        while data:
-            print(f"Получено: {data.decode()}")
-            client_socket.send(data)
-            data = client_socket.recv(1024)
-        
-        print("Подключение закрыто")
-        client_socket.close()
+
+    try:
+        server_socket.bind((host, port))
+        server_socket.listen(5)
+        print(f"Эхо-сервер запущен на {host}:{port}...")
+
+        while True:
+            client_socket, addr = server_socket.accept()
+            print(f"Подключено от {addr}")
+
+            try:
+                data = client_socket.recv(1024)
+                while data:
+                    print(f"Получено: {data.decode()}")
+                    client_socket.send(data)
+                    data = client_socket.recv(1024)
+
+                print("Подключение закрыто")
+            except Exception as e:
+                print(f"Ошибка при обработке подключения: {e}")
+            finally:
+                client_socket.close()
+
+    except Exception as e:
+        print(f"Произошла ошибка: {e}")
+    finally:
+        server_socket.close()
 
 if __name__ == "__main__":
     echo_server()
