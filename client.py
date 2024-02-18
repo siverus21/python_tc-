25819ddef5447b9aab295ca40382305fa053f731
g.skrylnikov <grisha2182@gmail.com>
2024-02-18 22:34:32 +0300

    fix 1

--- client.py
+++ client.py
@@ -1,46 +1,60 @@
 import socket
-import errno  # Импортируем модуль errno
+import errno 
 from command import *
-import time
 
-# Создание сокета
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def initialize_client():
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_address = ('localhost', 12345)
+    client_socket.connect(server_address)
+    return client_socket
 
-# Подключение к серверу
-server_address = ('localhost', 12345)
-client_socket.connect(server_address)
+def receive_command(client_socket):
+    try:
+        command = client_socket.recv(1024).decode()
+        print("Получена команда от сервера:", command)
+        return command
+    except:
+        return 'error'
 
-try:
-    while True:
-        try:
-            command = client_socket.recv(1024).decode()
-            print("Получена команда от сервера:", command)
+def send_response(client_socket, response):
+    try:
+        client_socket.sendall(response.encode())
+    except socket.error as e:
+        if e.errno == errno.EPIPE:
+            print("Соединение было разорвано")
 
-            if command == "END":
-                print("Получена команда завершения от сервера.")
-                break
-
-            if command != "photo":
-                command_parts = command.split(",")
-                action = command_parts[0]
-                try:
-                    value = int(command_parts[1])
-                except:
-                    response = "Указанное значение не является числом!"
-
-                if action in commands:
-                    response = "OK"
-                else:
-                    response = "Неверная команда"
-            elif command == "photo":
-                response = "OK"
-        except:
-            response = 'error'
-        try:
-            client_socket.sendall(response.encode())
-        except socket.error as e:
-            if e.errno == errno.EPIPE:
-                print("Соединение было разорвано")
-                break
-finally:
+def close_connection(client_socket):
     client_socket.close()
+
+def run_client():
+    client_socket = initialize_client()
+    try:
+        while True:
+            try:
+                command = receive_command(client_socket)
+
+                if command == "END":
+                    print("Получена команда завершения от сервера.")
+                    break
+
+                if command != "photo":
+                    command_parts = command.split(",")
+                    action = command_parts[0]
+                    try:
+                        value = int(command_parts[1])
+                    except:
+                        response = "Указанное значение не является числом!"
+
+                    if action in commands:
+                        response = "OK"
+                    else:
+                        response = "Неверная команда"
+                elif command == "photo":
+                    response = "OK"
+            except:
+                response = 'error'
+            send_response(client_socket, response)
+    finally:
+        close_connection(client_socket)
+
+run_client()
--- server.py
+++ server.py
@@ -2,60 +2,70 @@ import socket
 from command import *
 import time
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def initialize_server():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_address = ('localhost', 12345)
+    server_socket.bind(server_address)
+    server_socket.listen(1)
+    print("Ждем подключения клиента...")
+    client_socket, client_address = server_socket.accept()
+    print("Подключен клиент:", client_address)
+    return server_socket, client_socket
 
-server_address = ('localhost', 12345)
-server_socket.bind(server_address)
+def receive_command(client_socket):
+    try:
+        command = client_socket.recv(1024).decode()
+        print("Получена команда от сервера:", command)
+        return command
+    except:
+        return 'error'
 
-server_socket.listen(1)
+def send_response(client_socket, response):
+    try:
+        client_socket.sendall(response.encode())
+    except socket.error as e:
+        if e.errno == errno.EPIPE:
+            print("Соединение было разорвано")
 
-print("Ждем подключения клиента...")
-
-client_socket, client_address = server_socket.accept()
-print("Подключен клиент:", client_address)
-
-try:
-    height_now = CONST_START_HEIGHT
-    position_is_left = True
-    while height_now != CONST_END_HEIGHT:
-        angle_now = CONST_ANGLE_START
-        while angle_now <= CONST_ANGLE_END:
-
-            client_socket.sendall(Photo().encode())
-            response = client_socket.recv(1024).decode()
-
-            if CheckResponse(response) != "OK":
-                break
-
-            # time.sleep(0.3)
-            if(position_is_left):
-                client_socket.sendall(Right_step(defaul_step).encode())
-            else:
-                client_socket.sendall(Left_step(defaul_step).encode())
-
-            response = client_socket.recv(1024).decode()
-
-            if CheckResponse(response) != "OK":
-                break
-            else:
-                angle_now += defaul_step
-                print(angle_now)
-
-            # time.sleep(0.3)
-
-        if response != "OK":
-            client_socket.sendall(Up_step(abs(height_now)).encode())
-
-        position_is_left = not position_is_left
-
-        if(height_now == CONST_END_HEIGHT - 1):
-            client_socket.sendall(EndProgram().encode())
-            break
-        else:
-            height_now += 1
-            client_socket.sendall(Up_step(height_now).encode())
-            response = client_socket.recv(1024).decode()
-finally:
+def close_connections(server_socket, client_socket):
     time.sleep(1)
     client_socket.close()
     server_socket.close()
+
+def run_camera_control_server():
+    server_socket, client_socket = initialize_server()
+    try:
+        height_now = CONST_START_HEIGHT
+        position_is_left = True
+        while height_now != CONST_END_HEIGHT:
+            angle_now = CONST_ANGLE_START
+            while angle_now <= CONST_ANGLE_END:
+                client_socket.sendall(Photo().encode())
+                response = client_socket.recv(1024).decode()
+                if CheckResponse(response) != "OK":
+                    break
+                if position_is_left:
+                    client_socket.sendall(Right_step(defaul_step).encode())
+                else:
+                    client_socket.sendall(Left_step(defaul_step).encode())
+                response = client_socket.recv(1024).decode()
+                if CheckResponse(response) != "OK":
+                    break
+                else:
+                    angle_now += defaul_step
+                    print(angle_now)
+            if response != "OK":
+                client_socket.sendall(Up_step(abs(height_now)).encode())
+            position_is_left = not position_is_left
+            if height_now == CONST_END_HEIGHT - 1:
+                client_socket.sendall(EndProgram().encode())
+                break
+            else:
+                height_now += 1
+                client_socket.sendall(Up_step(height_now).encode())
+                response = client_socket.recv(1024).decode()
+    finally:
+        close_connections(server_socket, client_socket)
+
+# Вызовем функцию для запуска сервера
+run_camera_control_server()
